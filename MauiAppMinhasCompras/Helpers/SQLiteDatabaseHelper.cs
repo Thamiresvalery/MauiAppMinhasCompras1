dcb1eb25012434ea509d094dd156fb1437ffc478
thami <thamiresthsvs@gmail.com>
2025-03-14 21:01:36 -0300

    atualizacao

--- MauiAppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
+++ MauiAppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
@@ -34,9 +34,9 @@ namespace MauiAppMinhasCompras.Helpers
         {
             return _conn.Table<Produto>().ToListAsync();
         }
-        public Task<List<Produto>> Search(String q) 
+        public Task<List<Produto>> Search(string q) 
         {
-            string sql = "SELECT * Produto WHERE descricao LIKE '%" + q + "%'";
+            string sql = "SELECT * FROM Produto WHERE descricao LIKE '%" + q + "%'";
 
             return _conn.QueryAsync<Produto>(sql);
         }
