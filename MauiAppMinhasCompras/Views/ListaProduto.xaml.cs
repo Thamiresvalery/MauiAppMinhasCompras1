4d96bed42a176909d78dde7098fbf00c97819280
thami <thamiresthsvs@gmail.com>
2025-03-28 21:24:06 -0300

    atualizacoes app

--- MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -57,6 +57,11 @@ public partial class ListaProduto : ContentPage
         {
             await DisplayAlert("Ops", ex.Message, "OK");
         }
+        finally 
+        { 
+            lst_produtos.IsRefreshing = true;
+        }
+
     }
 
     private void ToolbarItem_Clicked_1(object sender, EventArgs e)
@@ -104,6 +109,26 @@ public partial class ListaProduto : ContentPage
         catch (Exception ex)
         {
              DisplayAlert("Ops", ex.Message, "OK");
+        }
+    }
+
+    private async void lst_produtos_Refreshing(object sender, EventArgs e)
+    {
+        try
+        {
+            lista.Clear();
+
+            List<Produto> tmp = await App.Db.GetAll();
+
+            tmp.ForEach(i => lista.Add(i));
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
+        finally
+        { 
+            lst_produtos.IsRefreshing = false;
         }
     }
 }
